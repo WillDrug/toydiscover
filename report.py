e7dc40cc2119198c434bd75b90abc81e22acb118
Бобков Сергей Александрович <s.bobkov@s7.ru>
2019-12-26 10:51:40 +0300

    switcharoo

---
+++ report.py
@@ -0,0 +1,21 @@
+# TODO: auto-downloaded code through some stuff
+
+import requests, threading
+
+class ToyDiscoverReporter:
+    WAIT_SECONDS = 25
+
+    def __init__(self, host, name, description):
+        self.ver = 1
+        self.host = host
+        self.name = name
+        self.description = description
+
+    def report(self):  # todo: param the URI up may be?
+        requests.post(f'http://toydiscover', json={'ver': self.ver, 'payload': {'host': self.host,
+                                                                               'name': self.name,
+                                                                               'description': self.description}})
+
+    def ioloop(self):
+        self.report()
+        threading.Timer(ToyDiscoverReporter.WAIT_SECONDS, self.ioloop).start()
